6ad3e9ee08dbad873d45c19b8580293e393aba21
Joe Joyave <jjoyave1@gmail.com>
2015-06-07 18:07:58 -0400

    got app working, with some styling.

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,39 @@
-console.log('This is cool!');
+// (function () {
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+//   'use strict';
+
+  var Todo = function(task) {
+    this.task = task;
+    this.status = (task.status !== undefined ? task.status : 'working');
+  };
+
+  var bin = {
+    "data": ""
+  };
+
+  var storage = [];
+  var $todoForm = $('#todo-form');
+
+  bin.data = storage;
+
+  $todoForm.on('submit', function(item) {
+
+    item.preventDefault();
+
+    var listItem = $('#todo-input').val();
+    var startTask = new Todo(listItem);
+
+    storage.push(startTask);
+
+    this.reset();
+
+    $('section').html(template.todolist(bin));
+
+  });
+
+// });
+
+$('#td-lnk').click(function(){
+  console.log('someone has clicked');
+  $(this).css('text-decoration', 'line-through');
+});
