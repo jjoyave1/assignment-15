95d6760933dff25e1813d162f92ed6713493e025
Joe Joyave <jjoyave1@gmail.com>
2015-06-07 19:39:37 -0400

    styled buttons, all click responses working

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@
 
   var Todo = function(task) {
     this.task = task;
-    this.status = (task.status !== undefined ? task.status : 'working');
+    this.status = (task.status !== undefined ? task.status : 'undone');
   };
 
   var bin = {
@@ -33,7 +33,10 @@
 
 // });
 
-$('#td-lnk').click(function(){
+$('section').on('click','li', function(){
   console.log('someone has clicked');
-  $(this).css('text-decoration', 'line-through');
+  $(this).css('background-color', '#520603');
+  $(this).css('opacity', '0.7');
+  $(this).parent().css('text-decoration', 'line-through');
+  $(this).status = 'done';
 });
--- app/js/templates.js
+++ app/js/templates.js
@@ -2,7 +2,7 @@ this["template"] = this["template"] || {};
 this["template"]["todolist"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper;
 
-  return "\n    <a href='#' id='td-lnk'><li id='td-btn'>"
+  return "\n    <a href='#'><li>"
     + this.escapeExpression(((helper = (helper = helpers.task || (depth0 != null ? depth0.task : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"task","hash":{},"data":data}) : helper)))
     + "</li></a>\n\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
