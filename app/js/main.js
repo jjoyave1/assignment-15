697b8b66810ce385fc62e7e09c47bff352db7ef1
Joe Joyave <jjoyave1@gmail.com>
2015-06-12 22:53:37 -0400

    reformed repository

---
+++ app/js/main.js
@@ -0,0 +1,53 @@
+// (function () {
+
+  // 'use strict';
+
+  var Todo = function(task) {
+    this.task = task;
+    this.status = (task.status !== undefined ? task.status : 'undone');
+  };
+
+  var bin = {
+    "data": ""
+  };
+
+  var storage = [];
+  var $todoForm = $('#todo-form');
+
+  bin.data = storage;
+
+  $todoForm.on('submit', function(item) {
+
+    item.preventDefault();
+
+    var listItem = $('#todo-input').val();
+    var startTask = new Todo(listItem);
+
+    storage.push(startTask);
+
+    this.reset();
+
+    $('section').html(template.todolist(bin));
+
+  });
+
+// });
+
+
+
+//prototype function
+
+$.fn.fadeOut = function(){
+
+  $(this).css('background-color', '#520603');
+
+  $(this).css('opacity', '0.7');
+
+  $(this).parent().css('text-decoration', 'line-through');
+
+  $(this).status = 'done';
+};
+
+$('section').on('click','li', function(){
+  $(this).fadeOut();
+});
